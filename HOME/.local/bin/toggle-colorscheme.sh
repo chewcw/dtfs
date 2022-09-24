8aff7efef33e7f01a515f3c9948cf803bbaa15ba
ChewCW <chwai87@gmail.com>
2022-09-24 13:38:20 +0800

    Update toggle-colorscheme.sh

--- HOME/.local/bin/toggle-colorscheme.sh
+++ HOME/.local/bin/toggle-colorscheme.sh
@@ -3,7 +3,7 @@
 set -e
 
 # https://github.com/toggle-corp/alacritty-colorscheme/blob/master/README.md
-export LIGHT_COLOR='base16-google-light.yml'
-export DARK_COLOR='base16-onedark.yml'
+export LIGHT_COLOR='base16-google-light-256.yml'
+export DARK_COLOR='base16-material-256.yml'
 $HOME/.local/bin/alacritty-colorscheme -V toggle $LIGHT_COLOR $DARK_COLOR
 
