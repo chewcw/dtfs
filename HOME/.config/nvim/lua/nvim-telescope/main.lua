9f4262d737610224fb17ff96570e5a62a2c9eab9
Chew Chee Wai <chew.cheewai@ynytechnology.com.my>
2023-02-27 08:53:01 +0800

    update

--- HOME/.config/nvim/lua/nvim-telescope/main.lua
+++ HOME/.config/nvim/lua/nvim-telescope/main.lua
@@ -46,9 +46,12 @@ telescope.load_extension("file_browser")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fb', function()
+  builtin.buffers({
+    initial_mode = "normal",
+  })
+end)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 vim.keymap.set("n", "<leader>fs", function()
   telescope.extensions.file_browser.file_browser({
     path = "%:p:h",
