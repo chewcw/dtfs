982d2e0eb781b9c95e2372755f40047435456b94
Chew Chee Wai <chew.cheewai@ynytechnology.com.my>
2023-05-21 18:48:31 +0800

    update install-nvim script

--- install-nvim.sh
+++ install-nvim.sh
@@ -4,18 +4,14 @@ set -e
 
 configFileDirectory=https://github.com/chewcw/dtfs/trunk/HOME/.config/nvim
 localConfigFilePath=$HOME/.config/nvim
-localConfigFile=init.vim
-localNvimPath=$HOME/.local/nvim
 
 parse_args() {
 	case $1 in
 		"full" )
 			install_dependency
 			install_nodejs
-			install_nvim_nightly
+			install_nvim_v083
 			configure_nvim
-			install_and_configure_plug
-			install_common_coc
 			;;
 		"update" )
 			configure_nvim
@@ -25,9 +21,8 @@ parse_args() {
 			;;
 		* )
 			install_dependency
-			install_nvim_nightly
+			install_nvim_v083
 			configure_nvim
-			install_and_configure_plug
 			;;
 	esac
 }
@@ -35,21 +30,16 @@ parse_args() {
 install_dependency() {
 	echo "installing dependencies"
 
-	# Ag
-	sudo apt update
-	[ ! $(command -v ag) ] && sudo apt install -y silversearcher-ag || true
 	# svn
 	[ ! $(command -v svn) ] && sudo apt install -y subversion || true
 	# ripgrep
 	[ ! $(command -v rg) ] && sudo apt install -y ripgrep || true
-
-	# Install fff
-	if [ ! -d "$HOME/.fff" ]; then
-		echo "cloning fff"
-		git clone https://github.com/dylanaraps/fff $HOME/.fff || true
-		sudo make -k -C $HOME/.fff install || true
-	fi
-
+  # xclip and xsel
+  [ ! $(command -v xclip ) ] && sudo apt install -y xclip xsel || true
+  # build-essential
+  sudo apt install -y build-essential || true
+  # python3-venv
+  sudo apt install -y python3-venv || true
 }
 
 install_nodejs() {
@@ -60,33 +50,18 @@ install_nodejs() {
 	echo "NodeJS is installed"
 }
 
-install_nvim_nightly() {
-	if ! command -v nvim &>/dev/null; then
-		echo "installing nvim nightly"
-		mkdir -p $localNvimPath
-		curl -Lo /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-		tar -xf /tmp/nvim-linux64.tar.gz -C /tmp/ || true
-		mv /tmp/nvim-linux64/* $localNvimPath || true
-		sudo ln -sf $localNvimPath/bin/nvim /usr/local/bin/nvim
-		rm -rf /tmp/nvim-linux64 || true
-	fi
-	echo "nvim nightly is installed"
-}
-
-install_and_configure_plug() {
-	echo "installing and configuring plug"
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' || true
-	$localNvimPath/bin/nvim -u $localConfigFilePath/$localConfigFile --headless +PlugInstall +qall &>/dev/null
-}
-
-install_common_coc() {
-	echo "installing common coc"
-	$localNvimPath/bin/nvim -u $localConfigFilePath/$localConfigFile --headless +'CocInstall -sync coc-go coc-pyright coc-tsserver' +qall &>/dev/null
+install_nvim_v083() {
+  if ! command -v nvim &>/dev/null; then
+    echo "installing nvim v0.8.3"
+    wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb -O /tmp/nvim-linux64.deb
+    sudo dpkg -i /tmp/nvim-linux64.deb
+  fi
+  echo "nvim v0.8.3 is installed"
 }
 
 configure_nvim() {
 	mkdir -p $localConfigFilePath
-	rm -rf $localConfigFilePath/$localConfigFile || true
+	rm -rf $localConfigFilePath/* || true
 	echo "svn checking out the nvim config directory from github"
 	cd $localConfigFilePath && svn checkout $configFileDirectory .
 }
@@ -94,8 +69,7 @@ configure_nvim() {
 uninstall() {
 	# remove neovim
 	echo "removing nvim"
-	rm -rf $localNvimPath || true
-	rm -rf /usr/local/bin/nvim || true
+  sudo apt remove --purge nvim
 	# remove neovim related files
 	echo "removing nvim related files"
 	rm -rf $localConfigFilePath || true
@@ -106,10 +80,10 @@ uninstall() {
 	rm -rf /usr/local/lib/node_modules || true
 	# remove svn
 	echo "removing svn"
-	sudo apt remove --purge subversion -y || true
-	# remove ag
-	echo "removing ag"
-	sudo apt remove --purge silversearcher-ag -y || true
+	sudo apt remove --purge subversion  -y || true
+	# remove python3-venv
+  echo "removing python3-venv"
+  sudo apt remove --purge  python3-venv -y || true
 }
 
 parse_args $1
