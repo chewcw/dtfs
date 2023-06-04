cc00df320a8819c3edf6c5c8798027f726aa3334
Chew Chee Wai <chew.cheewai@ynytechnology.com.my>
2023-06-04 14:29:42 +0800

    new development docker image and modification of the dev.sh script

--- HOME/.local/bin/dev.sh
+++ HOME/.local/bin/dev.sh
@@ -6,25 +6,18 @@
 set -e
 
 DOCKER=$(which docker)
-IMAGE=chewcw/dev:004
+IMAGE=chewcw/development
 TERM=$TERM
 DISPLAY=$DISPLAY
 
 $DOCKER run \
   -it \
   `# this is to make sure the neovim inside the container can have undercurl` \
+  `# must use alacritty in this case` \
   -e TERM=$TERM \
   `# these are for xclip/xsel to work inside the container` \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  --user 1000 \
-  `# working directory` \
-  --workdir /workspace \
-  `# used to run following commands` \
-  --entrypoint=bash \
   `# can append any desired argument` \
   $1 \
   $IMAGE \
-  -c "wget https://github.com/alacritty/alacritty/releases/download/v0.12.0/alacritty.info -O /tmp/alacritty.info && \
-    sudo tic -xe alacritty,alacritty-direct /tmp/alacritty.info && \
-    zsh"
