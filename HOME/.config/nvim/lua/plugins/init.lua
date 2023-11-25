00b3921931e49477f3f0c6dd166e1c0c064f16bf
chewcw <chwai87@gmail.com>
2023-11-25 23:52:34 +0800

    nvim-tree need not lazy loaded

--- HOME/.config/nvim/lua/plugins/init.lua
+++ HOME/.config/nvim/lua/plugins/init.lua
@@ -220,6 +220,7 @@ local default_plugins = {
   -- file managing , picker etc
   {
     "nvim-tree/nvim-tree.lua",
+    lazy = false,
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
     init = function()
       require("core.utils").load_mappings("nvimtree")
