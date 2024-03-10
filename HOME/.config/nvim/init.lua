6c7f4a0824139eb64dafa4e364a7a40e02ecf361
chewcw <chwai87@gmail.com>
2024-03-10 16:46:46 +0800

    Revert "Bootstrap lazy with command"
    This reverts commit afc73a19eda3c7fa35c4bf467c033ecf77dab763.

--- HOME/.config/nvim/init.lua
+++ HOME/.config/nvim/init.lua
@@ -2,16 +2,14 @@ require("core")
 
 require("core.utils").load_mappings()
 
-vim.api.nvim_create_user_command("LazyStart", function()
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-  -- bootstrap lazy.nvim on demand
-  if not vim.loop.fs_stat(lazypath) then
-    require("core.bootstrap").lazy(lazypath)
-  end
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+	require("core.bootstrap").lazy(lazypath)
+end
 
-  vim.opt.rtp:prepend(lazypath)
-  require("plugins")
-end, {})
+vim.opt.rtp:prepend(lazypath)
+require("plugins")
 
 require("core.colorscheme").setup()
