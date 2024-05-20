400d068a0ac9da5837733dbfea73693788d9eb79
ChewCW <chwai87@gmail.com>
2024-05-20 21:21:11 +0800

    Update couple of useful utility commands

---
+++ HOME/.local/bin/init_stuff.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+
+$HOME/.local/bin/connect_monitor.sh;
+
+xset r rate 300 70;
+
+fcitx &>/dev/null;
+
+sudo modprobe --remove psmouse;
+bash -c 'sleep 0.2; sudo modprobe psmouse'
+
+sudo timedatectl set-ntp false;
+bash -c 'sleep 0.2; timedatectl set-ntp true';
+
+pkill -f pasystray || true;
+bash -c 'sleep 0.2; pasystray &>/dev/null &';
+
+pkill -f xcompmgr || true;
+bash -c 'sleep 0.2; xcompmgr -c &>/dev/null &';
