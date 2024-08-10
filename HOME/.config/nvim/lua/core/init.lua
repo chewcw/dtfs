2bab78d2da53bda98f8b654d375d51957a36da1f
ChewCW <chwai87@gmail.com>
2024-08-10 11:27:03 +0800

    mkview on close not needed

--- HOME/.config/nvim/lua/core/init.lua
+++ HOME/.config/nvim/lua/core/init.lua
@@ -138,11 +138,11 @@ autocmd("FileType", {
 
 -- save fold on save and laod fold on open
 -- https://stackoverflow.com/a/77180744
-vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
-  pattern = { "*.*" },
-  desc = "save view (folds), when closing file",
-  command = "mkview",
-})
+-- vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+--   pattern = { "*.*" },
+--   desc = "save view (folds), when closing file",
+--   command = "mkview",
+-- })
 -- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 --   pattern = { "*.*" },
 --   desc = "load view (folds), when opening file",
