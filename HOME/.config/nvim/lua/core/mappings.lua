7f5643ae824f2a7cff5aff04524395fe890bcc3c
ChewCW <chwai87@gmail.com>
2024-08-16 15:46:27 +0800

    Disable <C-n> in insert mode

--- HOME/.config/nvim/lua/core/mappings.lua
+++ HOME/.config/nvim/lua/core/mappings.lua
@@ -54,6 +54,10 @@ M.general = {
     -- should I map this, this leave probably here for future reference?
     -- https://vim.fandom.com/wiki/Prevent_escape_from_moving_the_cursor_one_character_to_the_left
     -- ["<Esc>"] = { "<C-O>:stopinsert<CR>" },
+    -- This seems like autocomplete, disable this
+    ["<C-n>"] = { "" },
+    -- This seems like autocomplete, disable this
+    ["<C-p>"] = { "" },
   },
 
   n = {
