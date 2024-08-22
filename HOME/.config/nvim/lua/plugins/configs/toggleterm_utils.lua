ab9e0b742db2da59376e57b4d7699bec2e356999
ChewCW <chwai87@gmail.com>
2024-08-22 23:23:00 +0800

    Fix the toggleterm behavior

--- HOME/.config/nvim/lua/plugins/configs/toggleterm_utils.lua
+++ HOME/.config/nvim/lua/plugins/configs/toggleterm_utils.lua
@@ -1,39 +1,136 @@
 local M = {}
 
-M.toggle_term = function(direction)
-  if vim.g.toggle_term_direction == nil and vim.g.toggle_term_opened == nil then
-    vim.g.toggle_term_direction = direction
+M.toggle_term = function(target_direction)
+  if vim.g.toggle_term_direction == nil and vim.g.toggle_term_opened == nil and vim.g.toggle_term_count == nil then
+    vim.g.toggle_term_direction = target_direction
     vim.g.toggle_term_opened = false
+    vim.g.toggle_term_count = 1
   end
 
-  local count = vim.v.count or ""
+  local count = vim.v.count or 1
 
-  if vim.g.toggle_term_direction == direction and vim.g.toggle_term_opened == false then
-      vim.cmd(count .. "ToggleTerm direction=" .. direction)
+  if count ~= 0 then
+    if count ~= vim.g.toggle_term_count then
+      if vim.g.toggle_term_direction == target_direction then
+        if not vim.g.toggle_term_opened then
+          -- open term for direction [target_direction]
+          vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+          vim.g.toggle_term_opened = true
+          vim.g.toggle_term_count = count
+          goto done
+        end
+
+        if vim.g.toggle_term_opened then
+          -- close existing term
+          vim.cmd(vim.g.toggle_term_count .. "ToggleTerm")
+          vim.g.toggle_term_opened = false
+          -- open [count] term for same direction [target_direction]
+          vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+          vim.g.toggle_term_opened = true
+          vim.g.toggle_term_count = count
+          goto done
+        end
+        goto done
+      end
+
+      -- different direction
+      if not vim.g.toggle_term_opened then
+        -- open [count] term for different direction [target_direction]
+        vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+        vim.g.toggle_term_opened = true
+        vim.g.toggle_term_count = count
+        goto done
+      end
+
+      if vim.g.toggle_term_opened then
+        -- close existing term
+        vim.cmd(vim.g.toggle_term_count .. "ToggleTerm")
+        vim.g.toggle_term_opened = false
+        -- open [count] term for different direction [target_direction]
+        vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+        vim.g.toggle_term_opened = true
+        vim.g.toggle_term_count = count
+        goto done
+      end
+    else
+      if vim.g.toggle_term_direction == target_direction then
+        if not vim.g.toggle_term_opened then
+          -- open term for direction [target_direction]
+          vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+          vim.g.toggle_term_opened = true
+          vim.g.toggle_term_count = count
+          goto done
+        end
+
+        if vim.g.toggle_term_opened then
+          -- close the term
+          vim.cmd(vim.g.toggle_term_count .. "ToggleTerm")
+          vim.g.toggle_term_opened = false
+          vim.g.toggle_term_count = count
+          goto done
+        end
+        goto done
+      end
+
+      -- different direction
+      if not vim.g.toggle_term_opened then
+        -- open [count] term for different direction [target_direction]
+        vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+        vim.g.toggle_term_opened = true
+        vim.g.toggle_term_count = count
+        goto done
+      end
+
+      if vim.g.toggle_term_opened then
+        -- close existing term
+        vim.cmd(vim.g.toggle_term_count .. "ToggleTerm")
+        vim.g.toggle_term_opened = false
+        -- open [count] term for different direction [target_direction]
+        vim.cmd(count .. "ToggleTerm direction=" .. target_direction)
+        vim.g.toggle_term_opened = true
+        vim.g.toggle_term_count = count
+        goto done
+      end
+    end
+  else
+    if vim.g.toggle_term_direction == target_direction then
+      if not vim.g.toggle_term_opened then
+        -- open term for direction
+        vim.cmd("ToggleTerm direction=" .. target_direction)
+        vim.g.toggle_term_opened = true
+        goto done
+      end
+
+      if vim.g.toggle_term_opened then
+        -- close term
+        vim.cmd("ToggleTerm")
+        vim.g.toggle_term_opened = false
+        goto done
+      end
+      goto done
+    end
+
+    -- different direction
+    if not vim.g.toggle_term_opened then
+      -- open term for different direction
+      vim.cmd("ToggleTerm direction=" .. target_direction)
       vim.g.toggle_term_opened = true
-  elseif vim.g.toggle_term_direction == direction and vim.g.toggle_term_opened == true then
-    if count == "" then
+      goto done
+    end
+
+    if vim.g.toggle_term_opened then
+      -- close term first
       vim.cmd("ToggleTerm")
       vim.g.toggle_term_opened = false
-    end
-    if vim.g.toggle_term_count == count then
-      vim.cmd(count .. "ToggleTerm direction=" .. direction)
-      vim.g.toggle_term_opened = false
-    else
-      vim.cmd(count .. "ToggleTerm direction=" .. direction)
+      -- then open term for different direction
+      vim.cmd("ToggleTerm direction=" .. target_direction)
       vim.g.toggle_term_opened = true
+      goto done
     end
-  elseif vim.g.toggle_term_direction ~= direction and vim.g.toggle_term_opened == false then
-    vim.cmd(count .. "ToggleTerm direction=" .. direction)
-    vim.g.toggle_term_opened = true
-  elseif vim.g.toggle_term_direction ~= direction and vim.g.toggle_term_opened == true then
-    vim.cmd("ToggleTerm")
-    vim.g.toggle_term_opened = false
-    vim.cmd(count .. "ToggleTerm direction=" .. direction)
-    vim.g.toggle_term_opened = true
   end
-  vim.g.toggle_term_direction = direction
-  vim.g.toggle_term_count = count
+
+  ::done::
+  vim.g.toggle_term_direction = target_direction
 end
 
 return M
