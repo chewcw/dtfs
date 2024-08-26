749b95bc972a50dd6376f4a1d6839d3d81b97d6c
ChewCW <chwai87@gmail.com>
2024-08-26 09:31:16 +0800

    Update my connect_monitor command to also work with my own office monitor

--- HOME/.local/bin/connect_monitor.sh
+++ HOME/.local/bin/connect_monitor.sh
@@ -24,15 +24,24 @@ connect_monitor() {
 		hdmi=1
 	fi
 
+	yny_office_desk_monitor_edid="EDID: 00ffffffffffff0010ac4a4258474d41 2521010380351e78ea6785a854529f27"
+
 	if [[ $laptop_screen == 1 && $vga == 0 && $hdmi == 1 ]]; then
-    # work
+	# work in my office desk
+		monitor_prop=$(xrandr --prop | grep -A3 HDMI-1 | grep -A2 EDID | xargs)
+		if [[ "${monitor_prop}" == "${yny_office_desk_monitor_edid}" ]]; then
+			xrandr --output eDP-1 --mode 1920x1080 --primary --pos 0x0 \
+				--output HDMI-1 --mode 1920x1080 --pos 1920x0
+			return
+		fi
+		# work in other places (like meeting room, etc.)
 		xrandr --output eDP-1 --mode 1920x1080 --primary --pos 0x0 \
-      --output HDMI-1 --mode 1600x900 --pos 1920x0
+			--output HDMI-1 --mode 1600x900 --pos 1920x0
 	elif [[ $laptop_screen == 1 && $vga == 1 && $hdmi == 1 ]]; then
 		# home setup
-    xrandr --output eDP-1 --off \
-      --output DP-1 --mode 1920x1080 --primary --pos 0x0 \
-      --output HDMI-1 --mode 1920x1200 --pos 1920x0
+		xrandr --output eDP-1 --off \
+			--output DP-1 --mode 1920x1080 --primary --pos 0x0 \
+			--output HDMI-1 --mode 1920x1200 --pos 1920x0
 	else
 		xrandr --auto
 	fi
