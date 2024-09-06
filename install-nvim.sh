9dc5fa5c96c32062372a917f5c680a01e8a5bf5e
ChewCW <chwai87@gmail.com>
2024-09-06 20:45:09 +0800

    Update neovim to v0.10.1

--- install-nvim.sh
+++ install-nvim.sh
@@ -9,35 +9,35 @@ tempFilePath=/tmp/nvim-installation-temp
 mkdir -p $tempFilePath
 
 parse_args() {
-	case $1 in
-		"full" )
-			install_dependency
-			install_nodejs
-			install_nvim_v010
-			configure_nvim
+  case $1 in
+    "full" )
+      install_dependency
+      install_nodejs
+      install_nvim_v010
+      configure_nvim
       clean
-			;;
-		"update" )
-			configure_nvim
-			;;
-		"uninstall" )
-			uninstall
+      ;;
+    "update" )
+      configure_nvim
+      ;;
+    "uninstall" )
+      uninstall
       clean
-			;;
-		* )
-			install_dependency
-			install_nvim_v010
-			configure_nvim
+      ;;
+    * )
+      install_dependency
+      install_nvim_v010
+      configure_nvim
       clean
-			;;
-	esac
+      ;;
+  esac
 }
 
 install_dependency() {
-	echo "installing dependencies"
+  echo "installing dependencies"
 
-	# ripgrep
-	[ ! $(command -v rg) ] && sudo apt install -y ripgrep || true
+  # ripgrep
+  [ ! $(command -v rg) ] && sudo apt install -y ripgrep || true
   # xclip and xsel
   [ ! $(command -v xclip ) ] && sudo apt install -y xclip xsel || true
   # build-essential
@@ -47,28 +47,28 @@ install_dependency() {
 }
 
 install_nodejs() {
-	if ! command -v node &>/dev/null; then
-		echo "installing NodeJS"
-		curl -sL install-node.vercel.app/lts | sudo bash -s -- --yes
-	fi
-	echo "NodeJS is installed"
+  if ! command -v node &>/dev/null; then
+    echo "installing NodeJS"
+    curl -sL install-node.vercel.app/lts | sudo bash -s -- --yes
+  fi
+  echo "NodeJS is installed"
 }
 
 install_nvim_v010() {
   if ! command -v nvim &>/dev/null; then
-    echo "installing nvim v0.10.0"
-    wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz -O /tmp/nvim-linux64.tar.gz
+    echo "installing nvim v0.10.1"
+    wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz -O /tmp/nvim-linux64.tar.gz
     tar xvzf /tmp/nvim-linux64.tar.gz -C /tmp
-    sudo cp -r /tmp/nvim-linux64 /usr/local/nvim-v0-10-0
-    sudo ln -sf /usr/local/nvim-v0-10-0/bin/nvim /usr/bin/nvim
+    sudo cp -r /tmp/nvim-linux64 /usr/local/nvim-v0-10-1
+    sudo ln -sf /usr/local/nvim-v0-10-1/bin/nvim /usr/bin/nvim
   fi
-  echo "nvim v0.10.0 is installed"
+  echo "nvim v0.10.1 is installed"
 }
 
 configure_nvim() {
-	mkdir -p $localConfigFilePath
-	rm -rf $localConfigFilePath/* || true
-	echo "checking out the nvim config directory from github"
+  mkdir -p $localConfigFilePath
+  rm -rf $localConfigFilePath/* || true
+  echo "checking out the nvim config directory from github"
   cd $tempFilePath
   rm -rf dtfs/
   git clone https://github.com/chewcw/dtfs --no-checkout --depth 1 dtfs
@@ -80,18 +80,18 @@ configure_nvim() {
 }
 
 uninstall() {
-	# remove neovim
-	echo "removing nvim"
+  # remove neovim
+  echo "removing nvim"
   sudo apt remove --purge nvim
-	# remove neovim related files
-	echo "removing nvim related files"
-	rm -rf $localConfigFilePath || true
-	rm -rf $HOME/AppData/Local/nvim || true
-	# remove nodejs
-	echo "removing NodeJS"
-	sudo rm -rf /usr/local/bin/node /usr/local/bin/npm /usr/local/bin/npx || true
-	sudo rm -rf /usr/local/lib/node_modules || true
-	# remove python3-venv
+  # remove neovim related files
+  echo "removing nvim related files"
+  rm -rf $localConfigFilePath || true
+  rm -rf $HOME/AppData/Local/nvim || true
+  # remove nodejs
+  echo "removing NodeJS"
+  sudo rm -rf /usr/local/bin/node /usr/local/bin/npm /usr/local/bin/npx || true
+  sudo rm -rf /usr/local/lib/node_modules || true
+  # remove python3-venv
   echo "removing python3-venv"
   sudo apt remove --purge  python3-venv -y || true
 }
