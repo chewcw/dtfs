155e0989970741a48dde048ba8272c6ca414c52c
ChewCW <chwai87@gmail.com>
2024-09-17 10:58:46 +0800

    Update gitsigns configuration

--- HOME/.config/nvim/lua/plugins/configs/others.lua
+++ HOME/.config/nvim/lua/plugins/configs/others.lua
@@ -75,13 +75,22 @@ end
 
 M.gitsigns = {
   signs = {
-    add = { text = "│" },
-    change = { text = "│" },
+    add = { text = "+" },
+    change = { text = "±" },
     delete = { text = "_" },
     topdelete = { text = "‾" },
     changedelete = { text = "~" },
     untracked = { text = "┆" },
   },
+  signs_staged = {
+    add = { text = "+" },
+    change = { text = "±" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "┆" },
+  },
+  signs_staged_enable = true,
   on_attach = function(bufnr)
     utils.load_mappings("gitsigns", { buffer = bufnr })
   end,
