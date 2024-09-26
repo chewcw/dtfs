8c1a1fda565f9650c91f004c9092919df9018ed1
ChewCW <chwai87@gmail.com>
2024-09-26 12:44:16 +0800

    Save the window size for running Gll related command

--- HOME/.config/nvim/lua/plugins/configs/buffer_utils.lua
+++ HOME/.config/nvim/lua/plugins/configs/buffer_utils.lua
@@ -508,6 +508,16 @@ end
 
 -- Run Git custom user command when the buffer name matches
 M.run_git_related_when_the_buffer_name_matches = function()
+  -- Save window size
+  local win_widths = {}
+  local win_heights = {}
+  -- Iterate through each window and store their sizes
+  for i = 1, vim.fn.winnr('$') do
+    local win_id = vim.fn.win_getid(i)
+    win_widths[win_id] = vim.api.nvim_win_get_width(win_id)
+    win_heights[win_id] = vim.api.nvim_win_get_height(win_id)
+  end
+
   local buf_path = vim.api.nvim_buf_get_name(0)
   if buf_path:match("^/tmp/nvim%.ccw/") then
     vim.g.gll_reload_manually_or_open_new = true
@@ -515,6 +525,14 @@ M.run_git_related_when_the_buffer_name_matches = function()
     vim.cmd("wincmd k")
     vim.cmd("wincmd q")
     vim.cmd("wincmd p") -- make sure to focus on the Gll window
+
+  -- Restore the window sizes
+  for win_id, width in pairs(win_widths) do
+    if vim.api.nvim_win_is_valid(win_id) then
+      vim.api.nvim_win_set_width(win_id, width)
+      vim.api.nvim_win_set_height(win_id, win_heights[win_id])
+    end
+  end
   elseif buf_path:match("^fugitive://") then
     pcall(function()
       vim.notify("Fetching remote...")
