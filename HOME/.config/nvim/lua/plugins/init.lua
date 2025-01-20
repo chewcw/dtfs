fec838c27dec8ebab1486e67ce08384f0a02c35c
ChewCW <chwai87@gmail.com>
2025-01-20 12:40:05 +0800

    Trying out plugin codecompanion.nvim

--- HOME/.config/nvim/lua/plugins/init.lua
+++ HOME/.config/nvim/lua/plugins/init.lua
@@ -952,30 +952,53 @@ local default_plugins = {
     end,
   },
 
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   dependencies = {
+  --     {
+  --       "github/copilot.vim",
+  --       lazy = false,
+  --       init = function()
+  --         vim.g.copilot_no_map_tab = true
+  --         pcall(function()
+  --           vim.keymap.set("i", "<A-Tab>", 'copilot#Accept("\\<CR>")', {
+  --             expr = true,
+  --             replace_keycodes = false,
+  --             desc = "Accept suggestion",
+  --           })
+  --           vim.keymap.set("i", "<A-]>", "<Plug>(copilot-next)", { "Cycle to next suggestion" })
+  --           vim.keymap.set("i", "<A-[>", "<Plug>(copilot-previous)", { "Cycle to previous suggestion" })
+  --           vim.keymap.set("i", "<A-q>", "<Plug>(copilot-dismiss)", { "Dismiss suggestion" })
+  --         end)
+  --       end,
+  --     },
+  --   },
+  --   build = "make tiktoken",
+  --   opts = {},
+  --   lazy = false,
+  -- },
+
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      {
-        "github/copilot.vim",
-        lazy = false,
-        init = function()
-          vim.g.copilot_no_map_tab = true
-          pcall(function()
-            vim.keymap.set("i", "<A-Tab>", 'copilot#Accept("\\<CR>")', {
-              expr = true,
-              replace_keycodes = false,
-              desc = "Accept suggestion",
-            })
-            vim.keymap.set("i", "<A-]>", "<Plug>(copilot-next)", { "Cycle to next suggestion" })
-            vim.keymap.set("i", "<A-[>", "<Plug>(copilot-previous)", { "Cycle to previous suggestion" })
-            vim.keymap.set("i", "<A-q>", "<Plug>(copilot-dismiss)", { "Dismiss suggestion" })
-          end)
-        end,
-      },
-    },
-    build = "make tiktoken",
-    opts = {},
+    "olimorris/codecompanion.nvim",
     lazy = false,
+    opts = function()
+      return {
+        strategies = {
+          chat = {
+            adapter = "copilot",
+          },
+          inline = {
+            adapter = "copilot",
+          },
+          agent = {
+            adapter = "copilot",
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
+    end,
   },
 
   {
