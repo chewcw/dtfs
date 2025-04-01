d939aec4e57009dc0f7887669f3d2cb251e3d7aa
ChewCW <chwai87@gmail.com>
2025-04-01 08:09:54 +0800

    feat(nvim): add focus.nvim window exclusion rules
    Configure focus.nvim with specific window exclusion rules for certain
    filetypes (neo-tree) and buftypes (nofile, nowrite, quickfix, terminal)
    to prevent automatic resizing in special windows where it would be
    disruptive or unnecessary.
    
    Correlated to commit 2ed7def where focus.nvim was initially added and
    commit afd2f41 where UI options were configured.

--- HOME/.config/nvim/lua/plugins/init.lua
+++ HOME/.config/nvim/lua/plugins/init.lua
@@ -1354,6 +1354,33 @@ local default_plugins = {
   {
     "nvim-focus/focus.nvim",
     event = "BufEnter",
+    init = function()
+      local ignore_filetypes = { "neo-tree" }
+      local ignore_buftypes = { "nofile", "nowrite", "quickfix", "terminal" }
+      local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+      vim.api.nvim_create_autocmd("WinEnter", {
+        group = augroup,
+        callback = function(_)
+          if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+            vim.w.focus_disable = true
+          else
+            vim.w.focus_disable = false
+          end
+        end,
+        desc = "Disable focus.nvim for certain buftypes",
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = augroup,
+        callback = function(_)
+          if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+          else
+            vim.b.focus_disable = false
+          end
+        end,
+        desc = "Disable focus autoresize for FileType",
+      })
+    end,
     opts = {
       ui = {
         number = false,
