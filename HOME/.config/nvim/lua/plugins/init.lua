03a0ee1f9b80d19d20ae44c1225c33deac38cd17
ChewCW <chwai87@gmail.com>
2026-02-06 11:01:35 +0800

    refactor(flash): update mappings and search highlight
    Change Flash plugin keybinding from <leader>F to <leader>Ff for clarity.
    Comment out the remote Flash mapping to avoid conflicts. Adjust Search
    highlight group to use white foreground and dark brown background for
    better visibility.

--- HOME/.config/nvim/lua/plugins/init.lua
+++ HOME/.config/nvim/lua/plugins/init.lua
@@ -1974,9 +1974,9 @@ local default_plugins = {
       },
     },
     keys = {
-      { "<leader>F", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "<leader>Ff", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "<leader>R", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      -- { "<leader>R", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
