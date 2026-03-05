541c8d41c27bfedc6b8c5ca94eb9e4ef6ddd83fd
ChewCW <chwai87@gmail.com>
2026-03-05 18:24:18 +0800

    feat(flash): add 'g' to label exclude in char mode
    Update Flash plugin's char mode configuration to exclude the 'g' character
    from label hints. This refinement prevents conflicts with common navigation
    commands and enhances the accuracy of multi-line jumps, resulting in a
    smoother and more predictable editing experience.

--- HOME/.config/nvim/lua/plugins/init.lua
+++ HOME/.config/nvim/lua/plugins/init.lua
@@ -1967,7 +1967,7 @@ local default_plugins = {
       modes = {
         char = {
           multi_line = true,
-          label = { exclude = "hjkliardcvVsCoO" },
+          label = { exclude = "ghjkliardcvVsCoO" },
           char_actions = function(motion)
             return {
               [";"] = "next",   -- set to `right` to always go right
