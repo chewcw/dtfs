c08534261bd3199af3ca76da8da0ec5fcf657e7c
ChewCW <chwai87@gmail.com>
2026-08-01 12:45:52 +0800

    fix(nvim): correct on_exit toggleterm id bookkeeping

--- HOME/.config/nvim/lua/plugins/configs/toggleterm.lua
+++ HOME/.config/nvim/lua/plugins/configs/toggleterm.lua
@@ -59,20 +59,25 @@ local options = {
   on_exit = function(term, job, exit_code, name)
     -- more information in plugins/configs/toggleterm_utils.lua
     local utils = require("core.utils")
-    if utils.table_contains(vim.g.toggle_term_opened_term_ids, term.id) then
-      local x = vim.g.toggle_term_opened_term_ids
-      table.remove(x, term.id)
+    local x = vim.g.toggle_term_opened_term_ids
+    if utils.table_contains(x, term.id) then
+      -- table.remove takes an index, not a value; find the index first
+      for i, opened_term_id in ipairs(x) do
+        if opened_term_id == term.id then
+          table.remove(x, i)
+          break
+        end
+      end
       vim.g.toggle_term_opened_term_ids = x
     end
 
-    -- randomly pick one for the vim.g.toggle_term_count record
+    -- pick the first remaining id for the toggle_term_count record,
+    -- or 0 if none are left
+    vim.g.toggle_term_count = 0
     for _, opened_term_id in ipairs(vim.g.toggle_term_opened_term_ids) do
       vim.g.toggle_term_count = opened_term_id
       break
     end
-
-    -- if there is none left, assign 0 to vim.g.toggle_term_count
-    vim.g.toggle_term_count = 0
 
     -- set vim.g.toggle_term_opened to false
     vim.g.toggle_term_opened = false
