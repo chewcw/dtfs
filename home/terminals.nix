9715a549d48dce506ceb5b9520fe8e386e636849
ChewCW <chwai87@gmail.com>
2026-08-04 23:07:07 +0800

    refactor(nix): condition ghostty wrapper on non-NixOS Linux
    Only apply the nixGLDefault wrapper on non-NixOS Linux (Debian, Ubuntu,
    Fedora, Arch, WSL2) where Nix-hosted GL libs need exposing to the GL
    loader. On NixOS and macOS the raw ghostty package is used since native
    GL works there. Also drop the duplicate ghostty entry: ghosttyExe now
    yields a single package.

--- home/terminals.nix
+++ home/terminals.nix
@@ -2,20 +2,19 @@
 
 let
   # Ghostty built with Nix uses Nix-hosted GLib/OpenGL libs. On non-NixOS
-  # systems (Debian) it won't launch without a wrapper that exposes them to
-  # the GL loader. nixGLDefault handles this.
+  # Linux systems (Debian, Ubuntu, Fedora, Arch, WSL2) it won't launch without
+  # a wrapper that exposes them to the GL loader. nixGLDefault handles this.
   # https://ghostty.org/docs/install/binary
-  ghosttyWrapper = pkgs.writeShellScriptBin "ghostty" ''
-    exec ${nixGL.packages."${pkgs.system}".default}/bin/nixGLDefault \
-      ${pkgs.ghostty}/bin/ghostty "$@"
-  '';
+  # On NixOS and macOS, native Ghostty works without nixGL.
+  ghosttyExe = if pkgs.stdenv.isLinux && !pkgs.stdenv.isNixOS
+    then pkgs.writeShellScriptBin "ghostty" ''
+      exec ${nixGL.packages."${pkgs.system}".default}/bin/nixGLDefault \
+        ${pkgs.ghostty}/bin/ghostty "$@"
+    ''
+    else pkgs.ghostty;
 in
 {
-  home.packages = with pkgs; [
-    kitty
-    ghostty
-    ghosttyWrapper
-  ];
+  home.packages = [ ghosttyExe pkgs.kitty ];
 
   # Kitty config
   xdg.configFile."kitty/kitty.conf".source = ../HOME/.config/kitty/kitty.conf;
